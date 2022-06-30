428d19b18a903b157e4729c930c80f4ee5446e44
Vincent Wang <vwang26@massmutual.com>
2022-06-30 17:14:54 -0400

    first commit

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+//import * as Style from "../styles/indexStyle"
+
+const IndexPage = () => {
+  return (
+    <>
+      <div>Hey</div>
+    </>
+  )
+}
+
+export default IndexPage
