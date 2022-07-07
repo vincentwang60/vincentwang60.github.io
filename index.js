61b67c669a85a9551d657276ec3e4c35affceaa3
Vincent Wang <vincent.wang.60@gmail.com>
2022-07-07 19:12:02 -0400

    sexy wheel

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import * as React from "react"
 import Header from "../components/header"
+import Main from "../components/main"
 const css = require('../constants.css')
 
 // https://coolors.co/cb997e-f3e9e2-ffe8d6-b7b7a4-a5a58d-6b705c
 
 const IndexPage = () => {
-  
+
   return (
     <>
-      <Header/>
-      <div id="test">
-        Body
+      <Header />
+      <div style={{padding: '0.5vw', background: "#2B2C26"}}>
+        <Main />
       </div>
     </>
   )
